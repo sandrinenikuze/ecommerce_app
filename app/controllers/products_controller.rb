de1c38428ac263da7fd6e6e9c0103cf68cb3eecf
sandrinenikuze <nikuzesandrine@yahoo.fr>
2017-01-25 20:35:04 +0200

    Myfirst app

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,43 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+  end
+   def new
+     
+   end
+
+   def create 
+     name = params[:name]
+     price = params[:price]
+     description = params[:description]
+     image = params[:image]
+     product = Product.new ({name: name, price: price, description: description, image:image})
+     product.save 
+     flash[:success] = "Product Create"
+     redirect_to "/products"
+   end 
+
+   def edit
+     @product = Product.find_by(id: params[:id])
+   end
+   def update
+     product = Product.find_by(id: params[:id])
+     product.name = params[:name]
+     product.price =  params[:price]
+     product.description = params[:description]
+     product.image = params[:image]
+     product.save
+     redirect_to "/products"
+
+   end
+
+   def destroy
+     @product = Product.find_by(id: params[:id])
+     @product.destroy
+     redirect_to "/products"
+   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125090943) do
+ActiveRecord::Schema.define(version: 20170125100355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170125090943) do
     t.string   "name"
     t.integer  "price"
     t.string   "image"
+    t.string   "string"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
